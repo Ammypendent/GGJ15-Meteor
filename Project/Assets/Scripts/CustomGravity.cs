f9cfd5b81d34c4e207773de19ef99a575306846f
EnoKhaon <potandpans101@hotmail.com>
2015-01-23 23:46:06 -0800

    Custom Gravity Test
    Hopefully, just adding one file shouldn't be *too* detrimental.

---
+++ Project/Assets/Scripts/CustomGravity.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(Rigidbody))]
+
+public class CustomGravity : MonoBehaviour
+{
+
+	public Transform tGravityCenter;
+	Vector3 v3GravityCenter;
+
+	// Use this for initialization
+	void Start()
+	{
+		if(tGravityCenter != null)
+		{
+			v3GravityCenter = tGravityCenter.position;
+		}
+		else
+		{
+			v3GravityCenter = new Vector3(0, 0, 0);
+		}
+		rigidbody.useGravity = false;
+	}
+	
+	// Update is called once per frame
+	void Update()
+	{
+		
+	}
+
+	void FixedUpdate()
+	{
+		rigidbody.AddForce((v3GravityCenter - transform.position).normalized * Physics.gravity.magnitude, ForceMode.Acceleration);
+	}
+
+	void UpdateVelocity()
+	{
+		rigidbody.velocity *= -1;
+	}
+
+	void Teleport(Vector3 destination)
+	{
+		transform.position = destination;
+	}
+}
