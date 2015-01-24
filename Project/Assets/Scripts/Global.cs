f7eead26660baaf0ce7050f9cc0c69244a21ad3b
Ammypendent <jualco.hammerwing@gmail.com>
2015-01-24 12:17:26 -0800

    Added stuff.

---
+++ Project/Assets/Scripts/Global.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public static class Global {
+
+
+	public static bool GameRunning;
+
+	public static int playerOneHealth;
+	public static int playerTwoHealth;
+
+
+
+	public static float gameTime;
+
+
+
+	public static void StartGame ()
+	{
+		playerOneHealth = 20;
+		playerTwoHealth = 20;
+
+		gameTime = 0;
+
+		GameRunning = true;
+	}
+	
+	
+}
