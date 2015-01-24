7ad52bf040097f61b7395fa5617c30aefb63a973
BlueRider0 <adanielson@gmail.com>
2015-01-24 13:42:21 -0800

    Initial Gamepad code
    Includes XInput library.

--- Project/Assets/Scripts/Guns/RightGunControls.cs
+++ Project/Assets/Scripts/Guns/RightGunControls.cs
@@ -1,10 +1,18 @@
 ﻿using UnityEngine;
 using System.Collections;
 
+// XBox Gamepad library.
+using XInputDotNetPure;
+
 public class RightGunControls : MonoBehaviour {
 
 	public int gunSpeed;
 	public GameObject bullet;
+
+	private bool playerIndexSet = false;
+	private PlayerIndex playerIndex;
+	private GamePadState state;
+	private GamePadState prevState;
 
 	// Use this for initialization
 	void Start () {
@@ -13,6 +21,38 @@ public class RightGunControls : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
+
+		if (!playerIndexSet || !prevState.IsConnected)
+		{
+			for (int i = 0; i < 4; ++i)
+			{
+				PlayerIndex testPlayerIndex = (PlayerIndex)i;
+				GamePadState testState = GamePad.GetState(testPlayerIndex);
+				if (testState.IsConnected)
+				{
+					//Debug.Log(string.Format("GamePad found {0}", testPlayerIndex));
+					playerIndex = testPlayerIndex;
+					playerIndexSet = true;
+				}
+			}
+		}
+
+		prevState = state;
+		state = GamePad.GetState(playerIndex);
+
+		// Detect if a button was pressed this frame
+		if (prevState.Buttons.A == ButtonState.Released && state.Buttons.A == ButtonState.Pressed)
+		{
+			fireGun ();
+		}
+
+
+
+		if (state.ThumbSticks.Left.Y > 0.1 || state.ThumbSticks.Left.Y < -0.1)
+			moveGun (state.ThumbSticks.Left.Y);
+
+
+
 		if (Input.GetKey("up") && this.transform.position.y < 0.215)
 			this.transform.RotateAround (Vector3.zero, Vector3.forward, gunSpeed * Time.deltaTime);
 		
@@ -21,7 +61,19 @@ public class RightGunControls : MonoBehaviour {
 
 		if (Input.GetKeyDown ("return")) 
 		{
-			Instantiate(bullet, this.transform.position, Quaternion.identity);
+			fireGun ();
 		}
 	}
+
+	void moveGun(float speed)
+	{
+		if ((speed > 0 && this.transform.position.y < 0.215) || (speed < 0 && this.transform.position.y > -0.215))
+				this.transform.RotateAround (Vector3.zero, Vector3.forward, speed * gunSpeed * Time.deltaTime);
+	}
+
+	void fireGun()
+	{
+		Instantiate (bullet, this.transform.position, Quaternion.identity);
+	}
+
 }
