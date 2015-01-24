1b16a21750b3782d6f307604b672493a9aeca5d4
Oscareeno <oscar.rodriguez1993@yahoo.com>
2015-01-24 13:10:42 -0800

    menu stuff
    lots of menu stuff

---
+++ Project/Assets/Scripts/GUI/Ambient.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Ambient : MonoBehaviour {
+		// Your light gameObject here.
+		public Light light;
+		
+		// Array of random values for the intensity.
+		private float[] smoothing = new float[20];
+		private float startingIntensity;
+		private float timer;
+		private bool flickering = false;
+		void Start(){
+			// Initialize the array.
+			for(int i = 0 ; i < smoothing.Length ; i++){
+				smoothing[i] = .0f;
+			}
+			startingIntensity = light.intensity;
+			timer = Time.time + Random.Range(30,60)/10;
+		}
+		
+		void Update () {
+			float sum = .0f;
+			
+			// Shift values in the table so that the new one is at the
+			// end and the older one is deleted.
+			for(int i = 1 ; i < smoothing.Length ; i++)
+			{
+				smoothing[i-1] = smoothing[i];
+				sum+= smoothing[i-1];
+			}
+			
+			// Add the new value at the end of the array.
+			smoothing[smoothing.Length -1] = Random.value;
+			sum+= smoothing[smoothing.Length -1];
+			if (Time.time > timer) {
+				flickering = !flickering;
+				timer = Time.time + Random.Range(60,90)/10;
+			}
+			print (timer - Time.time);
+			// Compute the average of the array and assign it to the
+			// light intensity.
+			if (flickering)
+				light.intensity = sum / smoothing.Length;
+			else
+				light.intensity = startingIntensity;
+	}
+}
---
+++ Project/Assets/Scripts/GUI/StartMenu.cs
@@ -0,0 +1,107 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class StartMenu : MonoBehaviour {
+	private bool _isFirstMenu = true;
+	private bool _isLevelSelectMenu = false;
+	private bool _isLoadGameMenu = false;
+	private bool _isOptionsMenu = false;
+	public Texture title;
+	public Texture Controls;
+	void Start()
+	{
+	
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+	}
+	
+	void OnGUI()
+	{
+		GUI.contentColor = Color.green;
+		GUI.Label(new Rect(Screen.width / 2 - 230, Screen.height / 2 - 180 , title.width, title.height),title);
+		
+		FirstMenu();
+		LoadGameMenu();
+		LevelSelectMenu();
+		OptionsMenu();
+		
+		if(_isLevelSelectMenu == true || _isLoadGameMenu == true || _isOptionsMenu == true)
+		{
+			if(GUI.Button(new Rect(10, Screen.height - 35, 150, 25), "Back"))
+			{
+				_isLevelSelectMenu = false;
+				_isLoadGameMenu = false;
+				_isOptionsMenu = false;
+				_isFirstMenu = true;
+			}
+		}
+	}
+	
+	void FirstMenu()
+	{
+		if(_isFirstMenu)
+		{
+			if(GUI.Button(new Rect(Screen.width / 2 - 70, Screen.height / 2 - 100, 150, 25), "Begin Annihilation"))
+			{
+				Application.LoadLevel("Level01");
+			}
+			
+			if (GUI.Button(new Rect(Screen.width / 2 - 70, Screen.height / 2 - 65, 150, 25), "War Options"))
+			{
+				_isFirstMenu = false;
+				_isOptionsMenu = true;
+			}
+			
+			if (GUI.Button(new Rect(Screen.width / 2 - 70, Screen.height / 2 - 30, 150, 25), "Go AWOL"))
+			{
+				Application.Quit();
+			}
+			
+			/*if (GUI.Button(new Rect(10, Screen.height / 2 + 5, 150, 25), "Quit Game"))
+			{
+				_isFirstMenu = false;
+				_isOptionsMenu = true;
+			}
+			
+			if (GUI.Button(new Rect(10, Screen.height / 2 + 40, 150, 25), "Quit Game"))
+			{
+				Application.Quit();
+			}*/
+		}
+	}
+	
+	void LoadGameMenu()
+	{
+		if(_isLoadGameMenu)
+		{
+			
+		}
+	}
+	
+	void LevelSelectMenu()
+	{
+		if(_isLevelSelectMenu)
+		{
+			
+		}
+	}
+	
+	void OptionsMenu()
+	{
+		if(_isOptionsMenu)
+		{
+			if(GUI.Button(new Rect(Screen.width / 2 - 70, Screen.height / 2 - 100, 150, 25), "Control Schemes"))
+			{
+				GUI.Label(new Rect(Screen.width / 2 - 230, Screen.height / 2 - 180 , Controls.width / 2, Controls.height / 2),Controls);
+			}
+			if(GUI.Button(new Rect(Screen.width / 2 - 70, Screen.height / 2 - 50, 150, 25), "Engineers"))
+			{
+
+			}
+		}
+	}
+
+}
