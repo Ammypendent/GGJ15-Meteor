b715bea8c585decf5ba9f115ffbe4b2381fc6163
BlueRider0 <adanielson@gmail.com>
2015-01-24 00:41:24 -0800

    Control Scripts and Gun models
    And all the metadatas.

---
+++ Project/Assets/Scripts/RightGunControls.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RightGunControls : MonoBehaviour {
+
+	public int gunSpeed;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKey("up") && this.transform.position.y < 0.215)
+			this.transform.RotateAround (Vector3.zero, Vector3.forward, gunSpeed * Time.deltaTime);
+		
+		if (Input.GetKey("down") && this.transform.position.y > -0.215)
+			this.transform.RotateAround (Vector3.zero, Vector3.back, gunSpeed * Time.deltaTime);
+	}
+}
