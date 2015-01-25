43da79017ba15f8d497cb2b15f2263a3591e6069
Ammypendent <jualco.hammerwing@gmail.com>
2015-01-25 13:01:45 -0800

    Finished up the start menu and victory menu/credits. More final assets.

--- Project/Assets/Scripts/GUI/StartMenu.cs
+++ Project/Assets/Scripts/GUI/StartMenu.cs
@@ -13,21 +13,21 @@ public class StartMenu : MonoBehaviour {
 	}
 	private menuIs ourMenu;
 
-	private bool _isFirstMenu = true;
-	private bool _isFinishedMenu = false;
-	private bool _isControlMenu = false;
+
 	public Texture title;
 	public Texture Controls;
 	public Texture Controls1;
+	public Texture VictoryPlayerOne;
+	public Texture VictoryPlayerTwo;
+	public Texture CreditsPg1;
+	public Texture CreditsPg2;
+
 	private bool controlMenuKeyboard;
-	public static bool GameRunning;
-	public static bool GameFinished;
-	public static int playerOneHealth = 20;
-	public static int playerTwoHealth= 20;
 	string[] startMenuButtons= new string[3] {"Begin", "How To Play", "Quit"};
 	int selected = 0;
 
-	
+	private int finishedPage;
+
 	// Update is called once per frame
 	void Update () 
 	{
@@ -59,26 +59,42 @@ public class StartMenu : MonoBehaviour {
 				Global.GameFinished = false;
 			}
 		}
-		else if (ourMenu == menuIs.HUD)
-		{
 
+		//Ready to start game.
+		else if (ourMenu == menuIs.HUD && !Global.GameRunning)
+		{
+			Global.GameRunning = true;
 		}
 
-		if (Global.GameFinished) 
+		//If game is just finished
+		if (Global.GameFinished && Global.GameRunning) 
 		{
 			ourMenu = menuIs.Finished;
+			finishedPage = 0;
+			Global.GameRunning = false;	//Stop the game!
 		}
+
 
 	}
 	
 	void OnGUI()
 	{
 
-		GUI.DrawTexture(new Rect(0,0, Screen.width, Screen.height),title);// Produces the Title
 		GUI.contentColor = Color.green; // Changes the font to green
-		FirstMenu();
-		ControlMenu();
-		FinishedMenu();
+
+		if (ourMenu == menuIs.Start)
+		{
+			FirstMenu();
+		}
+		else if (ourMenu == menuIs.Control)
+		{
+			ControlMenu();
+		}
+		else if (ourMenu == menuIs.Finished)
+		{
+			FinishedMenu();
+		}
+
 
 		GUI.SetNextControlName(startMenuButtons[0]);
 		
@@ -90,337 +106,145 @@ public class StartMenu : MonoBehaviour {
 	void Start()
 	{
 		selected = 0;
+		finishedPage = 0;
 		controlMenuKeyboard = true;
+		ourMenu = menuIs.Start;
 	}
 
 
-		int menuSelection (string[] buttonsArray, int selectedItem, string direction) {
-			
-			if (direction == "up") {
-				
-				if (selectedItem == 0) {
-					
-					selectedItem = buttonsArray.Length - 1;
-					
-				} else {
-					
-					selectedItem -= 1;
-					
-				}
-				
+	int menuSelection (string[] buttonsArray, int selectedItem, string direction) 
+	{	
+		if (direction == "up") 
+		{	
+			if (selectedItem == 0) 
+			{	
+				selectedItem = buttonsArray.Length - 1;	
+			} 
+			else 
+			{
+				selectedItem -= 1;
 			}
-			
-			if (direction == "down") {
-				
-				if (selectedItem == buttonsArray.Length - 1) {
-					
-					selectedItem = 0;
-					
-				} else {
-					
-					selectedItem += 1;
-					
-				}
-				
-			}
-			
-			return selectedItem;
-			
 		}
-	
+		
+		if (direction == "down") 
+		{	
+			if (selectedItem == buttonsArray.Length - 1) 
+			{	
+				selectedItem = 0;	
+			} 
+			else 
+			{	
+				selectedItem += 1;	
+			}
+		}
+		return selectedItem;
+	}
+
+
 	void FirstMenu()
-	{
-		if (ourMenu == menuIs.Start) // Main Menu
-		{
-			GUI.SetNextControlName(startMenuButtons[0]);
-			if(GUI.Button(new Rect(Screen.width / 2 - 70, Screen.height / 2 - 125, 150, 50), startMenuButtons[0]))
-			{
-				_isFirstMenu = false;
-				Global.StartGame ();
-			}
-			GUI.SetNextControlName(startMenuButtons[1]);
-			
-			if(GUI.Button(new Rect(Screen.width / 2 - 70, Screen.height / 2 - 75, 150, 50), startMenuButtons[1]))
-			{			
-				ourMenu = menuIs.Control;
-			}
-			GUI.SetNextControlName(startMenuButtons[2]);
-			
-			if(GUI.Button(new Rect(Screen.width / 2 - 70, Screen.height / 2 - 25, 150, 50), startMenuButtons[2]))
-			{
-				Application.Quit();
-			}
-			GUI.SetNextControlName(startMenuButtons[0]);
-
-		/*if (GUI.Button (new Rect (Screen.width / 2 - 70, Screen.height / 2 - 125, 150, 50), "Begin Annihilation")) 
-			{
-					_isFirstMenu = false;
-					Global.StartGame ();
-			}
-
-			if (GUI.Button (new Rect (Screen.width / 2 - 70, Screen.height / 2 - 75, 150, 50), "How To Play")) 
-			{
-					_isFirstMenu = false;
-					_isControlMenu = true;
-			}
-
-			if (GUI.Button (new Rect (Screen.width / 2 - 70, Screen.height / 2 - 25, 150, 50), "Quit")) 
-			{
-					Application.Quit();
-			}*/
-
-			/*this is just for testing take out soon
-			if (GUI.Button (new Rect (Screen.width / 2 - 70, Screen.height / 2 - 10, 150, 25), "Finals")) 
-			{
-					_isFirstMenu = false;
-					_isFinishedMenu = true;
-					
-			}*/
-		}
-	}
-	
-	void ControlMenu()
-	{
-		if( ourMenu == menuIs.Control)
-		{
-			if(Input.GetKeyDown(KeyCode.Escape))
-			{
-				ourMenu = menuIs.Start;
-			}
-
-			GUI.contentColor = Color.white;
-			if (controlMenuKeyboard)
-			{
-				GUI.DrawTexture(new Rect(0, 0, (Screen.width), (Screen.height)),Controls);
-			}
-			else
-			{
-				GUI.DrawTexture(new Rect(0, 0, (Screen.width), (Screen.height)),Controls1);
-			}
-
-			if (GUI.Button (new Rect (Screen.width / 2 - 70, Screen.height / 2 - 310, 150, 50), "Main Menu")) 
-			{
-				ourMenu = menuIs.Start;
-			}
-			
-		}
-	}
-
-	void FinishedMenu()
-	{
-		if (_isFinishedMenu == true) 
-		{
-			if (GUI.Button (new Rect (Screen.width / 2, Screen.height - 60, 150, 25), "Continue")) 
-			{
-				ourMenu = menuIs.Start;
-			}
-		}
-	}
-}
-//=======
- /*using UnityEngine;
- using System.Collections;
-
-public class StartMenu : MonoBehaviour {
-
-
-	public enum menuIs
-	{
-		Start,
-		Control,
-		HUD,
-		Finished
-	}
-	private menuIs ourMenu;
-
-	private bool _isFirstMenu = true;
-	private bool _isFinishedMenu = false;
-	private bool _isControlMenu = false;
-	public Texture title;
-	public Texture Controls;
-	public Texture Controls1;
-	public static bool GameRunning;
-	public static bool GameFinished;
-	public static int playerOneHealth = 20;
-	public static int playerTwoHealth= 20;
-	string[] startMenuButtons= new string[3] {"Begin Annihilation", "How To Play", "Quit"};
-	int selected = 0;
-
-
-	// Update is called once per frame
-	void Update () 
-	{
-
-		if (ourMenu == menuIs.Start || ourMenu == menuIs.Control)
-		{
-			if(Input.GetKeyDown(KeyCode.W) || Input.GetKeyDown(KeyCode.DownArrow))
-			{
-				selected = menuSelection(startMenuButtons, selected, "up");
-			}
-
-			if(Input.GetKeyDown(KeyCode.S) || Input.GetKeyDown(KeyCode.UpArrow))
-			{
-				selected = menuSelection(startMenuButtons, selected, "down");
-			}
-		}
-		else if (ourMenu == menuIs.Finished)
-		{
-			if (Input.GetKeyDown(KeyCode.Return))// || Input.GetKeyDown(KeyCode.Space))
-			{
-				ourMenu = menuIs.Start;
-				Global.GameFinished = false;
-			}
-		}
-		else if (ourMenu == menuIs.HUD)
-		{
-
-		}
-
-		if (Global.GameFinished) 
-		{
-			ourMenu = menuIs.Finished;
-		}
-
-	}
-
-	void OnGUI()
 	{
 
 		GUI.DrawTexture(new Rect(0,0, Screen.width, Screen.height),title);// Produces the Title
-		GUI.contentColor = Color.green; // Changes the font to green
-		FirstMenu();
-		ControlMenu();
-		FinishedMenu();
 
 		GUI.SetNextControlName(startMenuButtons[0]);
-
-		GUI.FocusControl(startMenuButtons[selected]);
-
-		if(_isControlMenu == true)
+		if(GUI.Button(new Rect(Screen.width / 2 - 70, Screen.height / 2 - 125, 150, 50), startMenuButtons[0]))
 		{
-			if(GUI.Button(new Rect(Screen.width/ 2 - 70, Screen.height/ 2 -10, 150, 25), "Back"))
-			{
-				_isFinishedMenu = false;
-				_isControlMenu = false;
-				_isFirstMenu = true;
-			}
+			ourMenu = menuIs.HUD;
+			Global.StartGame ();
 		}
+		GUI.SetNextControlName(startMenuButtons[1]);
+		
+		if(GUI.Button(new Rect(Screen.width / 2 - 70, Screen.height / 2 - 75, 150, 50), startMenuButtons[1]))
+		{			
+			ourMenu = menuIs.Control;
+		}
+		GUI.SetNextControlName(startMenuButtons[2]);
+		
+		if(GUI.Button(new Rect(Screen.width / 2 - 70, Screen.height / 2 - 25, 150, 50), startMenuButtons[2]))
+		{
+			Application.Quit();
+		}
+		GUI.SetNextControlName(startMenuButtons[0]);
 
+
+		//final menu test sequence - remove soon
+		//if(GUI.Button(new Rect(Screen.width / 2 - 70, Screen.height / 2, 150, 50), "final test"))
+		//{			
+		//	ourMenu = menuIs.Finished;
+		//}
 
 	}
-		void Start(){
-			
-			selected = 0;
-			
-		}
-		int menuSelection (string[] buttonsArray, int selectedItem, string direction) {
-			
-			if (direction == "up") {
-				
-				if (selectedItem == 0) {
-					
-					selectedItem = buttonsArray.Length - 1;
-					
-				} else {
-					
-					selectedItem -= 1;
-					
-				}
-				
-			}
-			
-			if (direction == "down") {
-				
-				if (selectedItem == buttonsArray.Length - 1) {
-					
-					selectedItem = 0;
-					
-				} else {
-					
-					selectedItem += 1;
-					
-				}
-				
-			}
-			
-			return selectedItem;
-			
-		}
-
-	void FirstMenu()
-	{
-		if (_isFirstMenu) // Main Menu
-		{
-
-			if(GUI.Button(new Rect(Screen.width / 2 - 70, Screen.height / 2 - 125, 150, 50), startMenuButtons[0]))
-			{
-				_isFirstMenu = false;
-				Global.StartGame ();
-			}
-			GUI.SetNextControlName(startMenuButtons[1]);
-
-			if(GUI.Button(new Rect(Screen.width / 2 - 70, Screen.height / 2 - 75, 150, 50), startMenuButtons[1]))
-			{			
-				ourMenu = menuIs.Control;
-			}
-			GUI.SetNextControlName(startMenuButtons[2]);
-
-			if(GUI.Button(new Rect(Screen.width / 2 - 70, Screen.height / 2 - 25, 150, 50), startMenuButtons[2]))
-			{
-				Application.Quit();
-			}
-
-
-		/*if (GUI.Button (new Rect (Screen.width / 2 - 70, Screen.height / 2 - 125, 150, 50), "Begin Annihilation")) 
-			{
-					_isFirstMenu = false;
-					Global.StartGame ();
-			}
-
-			if (GUI.Button (new Rect (Screen.width / 2 - 70, Screen.height / 2 - 75, 150, 50), "How To Play")) 
-			{
-					_isFirstMenu = false;
-					_isControlMenu = true;
-			}
-
-			if (GUI.Button (new Rect (Screen.width / 2 - 70, Screen.height / 2 - 25, 150, 50), "Quit")) 
-			{
-					Application.Quit();
-			}*/
-
-			//this is just for testing take out soon
-			/*if (GUI.Button (new Rect (Screen.width / 2 - 70, Screen.height / 2 - 10, 150, 25), "Finals")) 
-			{
-					_isFirstMenu = false;
-					_isFinishedMenu = true;
-
-			}
-		}
-	}
-
+	
 	void ControlMenu()
 	{
-		if(_isControlMenu)
+		if(Input.GetKeyDown(KeyCode.Escape))
 		{
-			_isFirstMenu = false;
-			GUI.contentColor = Color.white;
+			ourMenu = menuIs.Start;
+		}
+
+		GUI.contentColor = Color.white;
+		if (controlMenuKeyboard)
+		{
 			GUI.DrawTexture(new Rect(0, 0, (Screen.width), (Screen.height)),Controls);
-			//GUI.Label(new Rect(0, 0, (Screen.width), (Screen.height)),Controls1);
+		}
+		else
+		{
+			GUI.DrawTexture(new Rect(0, 0, (Screen.width), (Screen.height)),Controls1);
+		}
 
-
+		if (GUI.Button (new Rect (Screen.width / 2 - 70, 55, 150, 50), "Main Menu")) 
+		{
+			ourMenu = menuIs.Start;
 		}
 	}
 
 	void FinishedMenu()
 	{
-		if (_isFinishedMenu == true) 
-		{
-			if (GUI.Button (new Rect (Screen.width / 2, Screen.height - 60, 150, 25), "Continue")) 
+		//if (Global.GameFinished) 
+		//{
+			if (finishedPage == 0)
+			{
+				//Who is the victor? Display the page of the winner
+				if (Global.playerOneHealth > 0)
+				{
+					GUI.DrawTexture(new Rect(0, 0, (Screen.width), (Screen.height)),VictoryPlayerOne);
+				}
+				else
+				{
+					GUI.DrawTexture(new Rect(0, 0, (Screen.width), (Screen.height)),VictoryPlayerTwo);
+				}
+
+				//Input to skip to next page
+				if (GUI.Button(new Rect(Screen.width / 2 - 70, 110, 150, 50), "continue"))
+				{
+					finishedPage++;
+				}
+			}
+			else if (finishedPage == 1)
+			{
+				GUI.DrawTexture(new Rect(0, 0, (Screen.width), (Screen.height)),CreditsPg1);
+
+				//Input to skip to next page
+				if (GUI.Button(new Rect(Screen.width / 2 - 70, 110, 150, 50), "continue"))
+				{
+					finishedPage++;
+				}
+			}
+			else if (finishedPage == 2)
+			{
+				GUI.DrawTexture(new Rect(0, 0, (Screen.width), (Screen.height)),CreditsPg2);
+
+				//Input to skip to next page
+				if (GUI.Button(new Rect(Screen.width / 2 - 70, 110, 150, 50), "continue"))
+				{
+					finishedPage++;
+				}
+			}
+			else //scrolled through all pages
 			{
 				ourMenu = menuIs.Start;
 			}
-		}
+
+		//}
 	}
 }
-
-*/
